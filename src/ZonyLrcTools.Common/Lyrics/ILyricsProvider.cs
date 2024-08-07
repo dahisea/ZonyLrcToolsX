2cc1b621c7741e17449b98d09701398fd979ac06
Dahi <81472122+dahisea@users.noreply.github.com>
2024-08-07 23:04:00 +0800

    Update ILyricsProvider.cs

--- src/ZonyLrcTools.Common/Lyrics/ILyricsProvider.cs
+++ src/ZonyLrcTools.Common/Lyrics/ILyricsProvider.cs
@@ -1,22 +1,8 @@
 namespace ZonyLrcTools.Common.Lyrics
 {
-    /// <summary>
-    /// 歌词数据下载器，用于匹配并下载歌曲的歌词。
-    /// </summary>
     public interface ILyricsProvider
     {
-        /// <summary>
-        /// 下载歌词数据。
-        /// </summary>
-        /// <param name="songName">歌曲的名称。</param>
-        /// <param name="artist">歌曲的作者。</param>
-        /// <param name="duration">歌曲的时长。</param>
-        /// <returns>歌曲的歌词数据对象。</returns>
-        ValueTask<LyricsItemCollection> DownloadAsync(string songName, string artist, string? duration = null);
-
-        /// <summary>
-        /// 下载器的名称。
-        /// </summary>
+        ValueTask<LyricsItemCollection> DownloadAsync(string songName, string artist, string songId, string? duration = null);
         string DownloaderName { get; }
     }
 }
