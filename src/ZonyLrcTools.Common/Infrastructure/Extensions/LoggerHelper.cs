b1df57579c9fcff37322ce60eb0f1e745fff9cc7
Dahi <81472122+dahisea@users.noreply.github.com>
2024-08-07 22:06:47 +0800

    Update LoggerHelper.cs

--- src/ZonyLrcTools.Common/Infrastructure/Extensions/LoggerHelper.cs
+++ src/ZonyLrcTools.Common/Infrastructure/Extensions/LoggerHelper.cs
@@ -1,53 +1,137 @@
-using System.Text;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using ZonyLrcTools.Common.Infrastructure.Exceptions;
-using ZonyLrcTools.Common.Infrastructure.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using ZonyLrcTools.Cli.Infrastructure.MusicScannerOptions;
+using ZonyLrcTools.Common;
+using ZonyLrcTools.Common.Album;
+using ZonyLrcTools.Common.Lyrics;
+using ZonyLrcTools.Common.MusicScanner;
 
-namespace ZonyLrcTools.Common.Infrastructure.Extensions
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace ZonyLrcTools.Cli.Commands.SubCommand
 {
-    /// <summary>
-    /// 日志记录相关的扩展方法。
-    /// </summary>
-    public static class LoggerHelper
+    [Command("download", Description = "下载歌词文件或专辑图像。")]
+    public class DownloadCommand : ToolCommandBase
     {
-        /// <summary>
-        /// 使用 <see cref="LogLevel.Warning"/> 级别打印错误日志，并记录异常堆栈。
-        /// </summary>
-        /// <param name="logger">日志记录器实例。</param>
-        /// <param name="errorCode">错误码，具体请参考 <see cref="ErrorCodes"/> 类的定义。</param>
-        /// <param name="e">异常实例，可为空。</param>
-        public static void LogWarningWithErrorCode(this IWarpLogger logger, int errorCode, Exception? e = null)
+        private readonly ILyricsDownloader _lyricsDownloader;
+        private readonly IAlbumDownloader _albumDownloader;
+        private readonly IMusicInfoLoader _musicInfoLoader;
+        private readonly IServiceProvider _serviceProvider;
+
+        public DownloadCommand(ILyricsDownloader lyricsDownloader,
+            IMusicInfoLoader musicInfoLoader,
+            IAlbumDownloader albumDownloader,
+            IServiceProvider serviceProvider)
         {
-            logger.WarnAsync($"错误代码: {errorCode}\n堆栈异常: {e?.StackTrace}").GetAwaiter().GetResult();
+            _lyricsDownloader = lyricsDownloader;
+            _musicInfoLoader = musicInfoLoader;
+            _albumDownloader = albumDownloader;
+            _serviceProvider = serviceProvider;
         }
 
-        /// <summary>
-        /// 使用 <see cref="LogLevel.Warning"/> 级别打印错误日志，并记录异常堆栈。
-        /// </summary>
-        /// <param name="logger">日志记录器的实例。</param>
-        /// <param name="exception">错误码异常实例。</param>
-        public static void LogWarningInfo(this IWarpLogger logger, ErrorCodeException exception)
+        #region > Options <
+
+        [Option("-d|--dir", CommandOptionType.SingleValue, Description = "指定需要扫描的目录。")]
+        [DirectoryExists]
+        public string SongsDirectory { get; set; }
+
+        [Option("-l|--lyric", CommandOptionType.NoValue, Description = "指定程序需要下载歌词文件。")]
+        public bool DownloadLyric { get; set; }
+
+        [Option("-a|--album", CommandOptionType.NoValue, Description = "指定程序需要下载专辑图像。")]
+        public bool DownloadAlbum { get; set; }
+
+        [Option("-n|--number", CommandOptionType.SingleValue, Description = "指定下载时候的线程数量。(默认值 2)")]
+        public int ParallelNumber { get; set; } = 1;
+
+        #endregion
+
+        #region > Scanner Options <
+
+        [Option("-sc|--scanner", CommandOptionType.SingleValue, Description = "指定歌词文件扫描器，目前支持本地文件(local)，网易云音乐(netease)，csv 文件(csv)，默认值为 local。")]
+        public string Scanner { get; set; } = "local";
+
+        [Option("-o|--output", Description = "指定歌词文件的输出路径。")]
+        public string OutputDirectory { get; set; } = "DownloadedLrc";
+
+        [Option("-p|--pattern", Description = "指定歌词文件的输出文件名模式。")]
+        public string OutputFileNamePattern { get; set; } = "{Artist} - {Name}.lrc";
+
+        [Option("-f|--file", Description = "指定 CSV 文件的路径。")]
+        public string CsvFilePath { get; set; }
+
+        [Option("-s|--song-list-id", Description = "指定网易云音乐歌单的 ID，如果有多个歌单，请使用 ';' 分割 ID。")]
+        public string SongListId { get; set; }
+
+        #endregion
+
+        protected override async Task<int> OnExecuteAsync(CommandLineApplication app)
         {
-            if (exception.ErrorCode < 50000)
+            if (!DownloadAlbum && !DownloadLyric)
             {
-                throw exception;
+                throw new ArgumentException("请至少指定一个下载选项，例如 -l(下载歌词) 或 -a(下载专辑图像)。");
             }
 
-            var sb = new StringBuilder();
-            sb.Append($"错误代码: {exception.ErrorCode}，信息: {ErrorCodeHelper.GetMessage(exception.ErrorCode)}");
-            sb.Append($"\n附加信息:\n {JsonConvert.SerializeObject(exception.AttachObject)}");
-            logger.WarnAsync(sb.ToString()).GetAwaiter().GetResult();
+            if (DownloadLyric)
+            {
+                await _lyricsDownloader.DownloadAsync(await GetMusicInfosAsync(Scanner), ParallelNumber);
+            }
+
+            if (DownloadAlbum)
+            {
+                await _albumDownloader.DownloadAsync(await GetMusicInfosAsync(Scanner), ParallelNumber);
+            }
+
+            return 0;
         }
 
         /// <summary>
-        /// 使用 <see cref="LogLevel.Information"/> 级别打印歌曲下载成功信息。
+        /// Get the music infos by the scanner.
         /// </summary>
-        /// <param name="logger">日志记录器的实例。</param>
-        /// <param name="musicInfo">需要打印的歌曲信息。</param>
-        public static void LogSuccessful(this IWarpLogger logger, MusicInfo musicInfo)
+        private async Task<List<MusicInfo>> GetMusicInfosAsync(string scanner)
         {
-            logger.InfoAsync($"歌曲名: {musicInfo.Name}, 艺术家: {musicInfo.Artist}, Sid: {NeLrequest.SongId}, 下载成功.").GetAwaiter().GetResult();
+            ValidateScannerOptions(scanner);
+
+            return scanner switch
+            {
+                MusicScannerConsts.LocalScanner => await _musicInfoLoader.LoadAsync(SongsDirectory, ParallelNumber),
+                MusicScannerConsts.CsvScanner => await _serviceProvider.GetService<CsvFileMusicScanner>()
+                    .GetMusicInfoFromCsvFileAsync(CsvFilePath, OutputDirectory, OutputFileNamePattern),
+                MusicScannerConsts.NeteaseScanner => await _serviceProvider.GetService<NetEaseMusicSongListMusicScanner>()
+                    .GetMusicInfoFromNetEaseMusicSongListAsync(SongListId, OutputDirectory, OutputFileNamePattern),
+                _ => await _musicInfoLoader.LoadAsync(SongsDirectory, ParallelNumber)
+            };
+        }
+
+        /// <summary>
+        /// Manually validate the options.
+        /// </summary>
+        /// <param name="scanner">Scanner Name.</param>
+        /// <exception cref="ArgumentException">If the options are invalid.</exception>
+        private void ValidateScannerOptions(string scanner)
+        {
+            if (scanner != MusicScannerConsts.LocalScanner && string.IsNullOrEmpty(OutputDirectory))
+            {
+                throw new ArgumentException("当使用非本地文件扫描器时，必须指定歌词文件的输出路径。");
+            }
+
+            if (scanner != MusicScannerConsts.LocalScanner && !Directory.Exists(OutputDirectory))
+            {
+                throw new ArgumentException("指定的歌词文件输出路径不存在。");
+            }
+
+            switch (scanner)
+            {
+                case MusicScannerConsts.CsvScanner when string.IsNullOrWhiteSpace(CsvFilePath):
+                    throw new ArgumentException("当使用 CSV 文件扫描器时，必须指定 CSV 文件的路径。");
+                case MusicScannerConsts.NeteaseScanner when string.IsNullOrWhiteSpace(SongListId):
+                    throw new ArgumentException("当使用网易云音乐扫描器时，必须指定歌单的 ID。");
+            }
         }
     }
 }
