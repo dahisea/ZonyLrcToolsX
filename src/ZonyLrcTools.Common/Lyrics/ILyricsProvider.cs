77b06162e2455d2c78a6a52dac5e56abeaf8cece
Dahi <81472122+dahisea@users.noreply.github.com>
2024-08-08 14:55:11 +0800

    6

--- src/ZonyLrcTools.Common/Lyrics/ILyricsProvider.cs
+++ src/ZonyLrcTools.Common/Lyrics/ILyricsProvider.cs
@@ -1,10 +1,10 @@
+using System.Threading.Tasks;
+
 namespace ZonyLrcTools.Common.Lyrics
 {
-public interface ILyricsProvider
-{
-    string DownloaderName { get; }
-
-    Task<LyricsItemCollection> DownloadAsync(string songName, string artist, string songId);
-}
-
+    public interface ILyricsProvider
+    {
+        string DownloaderName { get; }
+        ValueTask<LyricsItemCollection> DownloadAsync(string songName, string artist, string songId, string? duration = null);
+    }
 }
